b0dc16f09691a041ec8142b55f7bb07dea6603de
SaiMulagan <ssmulagan@dons.usfca.edu>
2023-11-09 20:44:15 -0800

    Requirement 1 done with PropertyManager file

---
+++ PropertyManager.java
@@ -0,0 +1,34 @@
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class PropertyManager {
+    private Properties properties;
+
+    public PropertyManager(String propertiesFilePath) {
+        properties = new Properties();
+        // Set default values
+        setDefaultValues();
+
+        if (propertiesFilePath != null) {
+            try (FileInputStream fis = new FileInputStream(propertiesFilePath)) {
+                properties.load(fis);
+            } catch (IOException e) {
+                System.out.println("Unable to read properties file, using default values.");
+            }
+        }
+    }
+
+    private void setDefaultValues() {
+        properties.setProperty("structures", "linked");
+        properties.setProperty("floors", "32");
+        properties.setProperty("passengers", "0.03");
+        properties.setProperty("elevators", "1");
+        properties.setProperty("elevatorCapacity", "10");
+        properties.setProperty("duration", "500");
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
